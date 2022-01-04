06cbcb351f8c4e35b60f682e1e059fcb52efd526
Conrad Bekondo <philllittle302@gmail.com>
2022-01-04 13:19:00 +0100

    fixed issues

--- index.mjs
+++ index.mjs
@@ -1,11 +1,11 @@
 import http from 'http';
-import ws from 'ws';
+import {WebSocketServer} from 'ws';
 
 const serverPort = parseInt(process.env.SERVER_PORT || '80');
 const maxUsers = process.env.MAX_USERS ? parseInt(process.env.MAX_USERS) : Number.POSITIVE_INFINITY;
 const loadBalancerHost = new URL(process.env.LOAD_BALANCER_HOST);
 
-let currentLoad = 0;
+let currentUserCount = 0;
 
 function date() {
     return new Date(Date.parse(new Date().toUTCString())).toISOString();
@@ -15,17 +15,32 @@ const server = http.createServer().listen(serverPort, () => {
     console.log(date(), `Server started on port: ${serverPort}`);
 });
 
-const wss = new ws.WebSocketServer({
+const wss = new WebSocketServer({
     server
 });
 
+let loadBalancerSocket;
+
 wss.on('connection', (socket, request) => {
-    const hostUrl = new URL(request.headers.host);
-    if (hostUrl.host == loadBalancerHost.host) {
+    if (request.headers.host == loadBalancerHost.host) {
         socket.on('ping', () => {
-            socket.pong(JSON.stringify({ currentLoad }));
+            socket.pong(JSON.stringify({ currentLoad: currentUserCount }));
         });
+        loadBalancerSocket = socket;
     } else {
+        if (currentUserCount >= maxUsers) {
+            socket.close(1016, `Saturation limit reached`);
+            return;
+        }
+        socket.on('close', code => {
+            if (code === 1001) {
+                currentUserCount--;
+                loadBalancerSocket?.send(JSON.stringify({ action: 'USER_LEFT', currentLoad: currentUserCount }));
+            }
+        });
+
+        currentUserCount++;
+        loadBalancerSocket?.send(JSON.stringify({ action: 'USER_ADDED', currentLoad: currentUserCount }));
         socket.on('message', data => {
             if (!data) return;
             console.log(date(), `Message received: ${data.toString()}`);
